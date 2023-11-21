e084a1286686a882acbe8de75e12ff549c697492
techwebdev <amit.kadivar3@gmail.com>
2023-11-21 20:22:43 +0530

    work on changes

--- front/src/components/Detail/Detail.js
+++ front/src/components/Detail/Detail.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../Home/Navbar'
 import Menu from '../Home/Menu'
 import Contact from '../Contact/Contact'
@@ -6,7 +6,7 @@ import Slideshow from './Slideshow'
 import Slideshowb from './Slideshow-b'
 import Related from './Related'
 import axios from 'axios'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import './Detail.css'
 
 function Detail() {
@@ -16,7 +16,7 @@ function Detail() {
     const [properties, setProperties] = useState([])
     const [related, setRelated] = useState([])
     const params = useParams()
-    useEffect(  () => {
+    useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.post(`${process.env.REACT_APP_ENDPOINT}fetch_detail`, params)
@@ -29,34 +29,34 @@ function Detail() {
         }
         const fetchRelated = async () => {
             try {
-                const response = await axios.post(`${process.env.REACT_APP_ENDPOINT}fetch_related`, {cat:category})
+                const response = await axios.post(`${process.env.REACT_APP_ENDPOINT}fetch_related`, { cat: category })
                 console.log(category)
-              
+
                 setProperties(response.data)
                 setRelated(response.data)
             } catch (error) {
                 console.log(error)
             }
         }
-        
-        if(Object.keys(property).length === 0 ) {
-            fetchRelated() 
+
+        if (Object.keys(property).length === 0) {
+            fetchRelated()
         }
 
         fetchData()
 
-        
+
     }, [])
     return (
         <div>
-            
-            <Menu/>
+
+            <Menu />
             <div className="img-name-container">
                 <div className="img-container">
                     <img src={property.portrait} alt="detail"></img>
-                 </div>            
+                </div>
             </div>
-            <Navbar/>
+            <Navbar />
             <div className="detail-container">
                 <div className="description-container">
                     <h2>{property.name}</h2>
@@ -75,7 +75,7 @@ function Detail() {
                     }
                     <div className="stat">
                         <h4>Precio</h4>
-                        <h3>{(property.price == 0) ? <u><a href={'mailto:crdr.jc@gmail.com?subject=Consulta Precio de la Propiedad: '+property.name}>Preguntar</a></u> : property.price}</h3>
+                        <h3>{(property.price == 0) ? <u><a href={'mailto:rfsm@landlatinamerica.com?subject=Consulta Precio de la Propiedad: ' + property.name}>Preguntar</a></u> : property.price}</h3>
                     </div>
                     <div className="stat">
                         <h4>Pais</h4>
@@ -84,20 +84,20 @@ function Detail() {
                 </div>
             </div>
             <div className="buyers-container">
-                <a href={'mailto:crdr.jc@gmail.com?subject=Consulta Propiedad: '+property.name}><button>Buyers Agent</button></a> 
+                <a href={'mailto:rfsm@landlatinamerica.com?subject=Consulta Propiedad: ' + property.name}><button>Buyers Agent</button></a>
             </div>
-            
+
             <div className="slideshow-container">
-                <Slideshowb /> 
+                <Slideshowb />
                 {/* <Slideshow />  */}
-                
+
             </div>
-            { property.video &&
+            {property.video &&
                 <div className="video-container">
                     <iframe width="700" height="465" src={property.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             }
-        
+
             <div className="map-container">
                 <div className="mapouter">
                     <div className="gmap_canvas">
@@ -109,10 +109,10 @@ function Detail() {
                 <h2>Propiedades relacionadas</h2>
                 <div className="related-container">
                     {
-                        related.filter((e)=> e.category_id == property.category_id).slice(0, 3).map((property) => {
+                        related.filter((e) => e.category_id == property.category_id).slice(0, 3).map((property) => {
                             return (
                                 <div className="related-item" key={property.id}>
-                                    <a href={'/detail/'+property.id}>
+                                    <a href={'/detail/' + property.id}>
                                         <img src={property.images[0].url}></img>
                                         <h3>{property.name}</h3>
                                     </a>
@@ -121,27 +121,12 @@ function Detail() {
                         })
                     }
                 </div>
-            </div> 
-        
-            <Contact/>
+            </div>
+
+            <Contact />
         </div>
     )
 }
 
 export default Detail
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
